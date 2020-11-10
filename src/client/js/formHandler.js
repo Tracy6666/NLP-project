03204a4c9919a00e4ab91a3e9c30f9767053ee0f
Tracy Chen <Tracy6666@users.noreply.github.com>
2020-11-09 22:28:36 -0500

    formText update

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -11,12 +11,51 @@ function handleSubmit(event) {
     return fetch(`${baseUrl}/sentiment?text=${formText}`)
         .then(res => res.json())
         .then(function(res) {
-
-            document.getElementById('results').innerHTML = res.message
+            updateUI(res, formText);
+            //document.getElementById('results').innerHTML = res.message
         })
         .catch((error) => {
             document.getElementById("results").innerHTML = error;
         });
 }
 
-export { handleSubmit }
+
+function getList(list) {
+    const listItems = [];
+    if (list) {
+        if (list.length > 0) {
+            list.forEach((item) => {
+                const htmlItem = `
+                <tr>
+                    <td>${item.form}</td>
+                    <td>${item.type}</td>
+                    <td>${item.score_tag}</td>
+                </tr>
+            `;
+                listItems.push(htmlItem);
+            });
+        }
+    }
+    if (listItems.length > 0) {
+        return `<div> $ {listItems.join(" ")}
+        </div>`;
+    } else {
+        return "<span>No items found</span>";
+    }
+}
+
+function updateUI(data, formText) {
+    const sentimentedEntityList = `<div> ${getList(data.sentimented_entity_list)} </div>`;
+    const sentimentedConceptList = `
+    <div class='result-section'>
+        <h2>Sentimented concept list</h2>
+        ${getList(data.sentimented_concept_list)}
+    </div>
+    `;
+
+    const htmlOutput = `
+        ${sentimentedEntityList}
+        ${sentimentedConceptList}
+    `;
+}
+export { handleSubmit };
